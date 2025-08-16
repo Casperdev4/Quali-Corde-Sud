74c78e7e62113c07cfd9b91ce097c7457e293e9e
Casperdev4 <allam.bilal91@gmail.com>
2025-08-16 17:09:53 +0200

    d

--- traitement.php
+++ traitement.php
@@ -65,7 +65,7 @@ try {
 
     $mail->setFrom('contact@webprime.fr', 'Quali Corde');
     $mail->addAddress('contacts@quali-corde.com');
-    $mail->addAddress('lennygui91@hotmail.com');
+    $mail->addAddress('webprime91@hotmail.com');
     $mail->CharSet = 'UTF-8';
     $mail->isHTML(true);
     $mail->Subject = 'Formulaire';
